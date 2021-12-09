e3b00207398993e7ec0a60a4413d3e0d4a788a6f
evanshango <evansonshango@gmail.com>
2021-12-09 23:54:59 +0300

    update pom file

--- src/main/java/com/codewithevans/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/codewithevans/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,10 +2,11 @@ package com.codewithevans.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
