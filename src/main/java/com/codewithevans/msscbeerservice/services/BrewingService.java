63b79e19bb989d68dde819257fe1f13c84796bff
evanshango <evansonshango@gmail.com>
2021-12-12 19:16:34 +0300

    implementing brewing service. closes #25

---
+++ src/main/java/com/codewithevans/msscbeerservice/services/BrewingService.java
@@ -0,0 +1,50 @@
+package com.codewithevans.msscbeerservice.services;
+
+import com.codewithevans.msscbeerservice.config.JmsConfig;
+import com.codewithevans.msscbeerservice.events.BeerEvent;
+import com.codewithevans.msscbeerservice.repositories.BeerRepository;
+import com.codewithevans.msscbeerservice.services.inventory.BeerInventoryService;
+import com.codewithevans.msscbeerservice.web.mappers.BeerMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+
+    private final BeerRepository beerRepository;
+    private BeerInventoryService inventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Autowired
+    public void setBeerInventoryService(
+            @Qualifier("beerInventoryServiceRestTemplateImpl") BeerInventoryService beerInventoryService
+    ) {
+        this.inventoryService = beerInventoryService;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void checkForLowInventory() {
+        var beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer quantityOnHand = inventoryService.getOnHandInventory(beer.getId());
+
+            log.debug("Min OnHand is: " + beer.getMinOnHand());
+            log.debug("Inventory is: " + quantityOnHand);
+
+            if (beer.getMinOnHand() >= quantityOnHand) {
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BeerEvent(beerMapper.beerToBeerDto(beer)));
+            }
+        });
+
+
+    }
+}
--- src/test/java/com/codewithevans/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/com/codewithevans/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,13 +1,12 @@
 package com.codewithevans.msscbeerservice.services.inventory;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+//@Disabled
 @SpringBootTest
 class BeerInventoryServiceRestTemplateImplTest {
 
